dddfee5260033bed187bf590fc699793d191b94f
Jerome Fabricante <fabricantejerome@gmail.com>
2024-08-12 11:13:11 +0800

    Show the active menu

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 const toggleMenu = () => {
-    
     const burgerMenu = document.querySelector(".menu-icon");
     const src = burgerMenu.getAttribute('src');
-    const iconName = src === './assets/burger.svg' ?
-        './assets/close.svg'
-        :
-        './assets/burger.svg';
+    const iconName = src === './assets/burger.svg' ? './assets/close.svg' : './assets/burger.svg';
 
-
-    burgerMenu.setAttribute(
-        'src',
-        iconName
-    );
+    burgerMenu.setAttribute('src', iconName);
 
     const navigation = document.querySelector('.navigation');
 
-    navigation.classList.toggle(
-        'navigation--mobile'
-    );
+    navigation.classList.toggle('navigation--mobile');
 };
 
+// Show section when scroll
 window.addEventListener('scroll', () => {
     const sections = document.querySelectorAll('section');
     sections.forEach(section => {
         const sectionTop = section.getBoundingClientRect().top;
         const sectionVisible = window.innerHeight / 2;
-        console.log(`Section top: ${sectionTop}, Section visible: ${sectionVisible}`)
+
         if (sectionTop < window.innerHeight - sectionVisible) {
             section.classList.add('fade-up');
         } else {
             section.classList.remove('fade-up');
         }
+    });
+});
+
+
+// Highlight active menu item on scroll and click
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    const changeActiveLink = () => {
+        let index = sections.length;
+
+        while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+        navLinks.forEach((link) => link.classList.remove('active'));
+        navLinks[index].classList.add('active');
+    };
+
+    changeActiveLink();
+    window.addEventListener('scroll', changeActiveLink);
+
+    navLinks.forEach((link) => {
+        link.addEventListener('click', function() {
+            console.log(this)
+            navLinks.forEach((link) => link.classList.remove('active'));
+            this.classList.add('active');
+        });
     });
 });
